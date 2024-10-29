04b1399cbfd571906c45a77325d669fd62cd09ff
Karimah Azzuhdu T.M <43397349+KarimahAzzuhdu@users.noreply.github.com>
2024-10-29 13:26:19 +0700

    add result test html page

--- myskill_practice_test.js
+++ myskill_practice_test.js
@@ -1,6 +1,10 @@
 import { check, group } from "k6"
 import http from "k6/http"
 
+// This will export to HTML as filename "result.html" AND also stdout using the text summary
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+
 export default function() {
     group('Reqres create user', () => {
         let url = "https://reqres.in/api/users"
@@ -21,4 +25,11 @@ export default function() {
             })
         })
     })
+}
+
+export function handleSummary(data) {
+    return {
+        "myskill_practice_result.html": htmlReport(data),
+        stdout: textSummary(data, { indent: " ", enableColors: true }),
+    };
 }
